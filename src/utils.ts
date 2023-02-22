42525348a072cb559e7afa907cdf579071200657
ReaJason <reajason1225@gmail.com>
2023-02-22 20:34:43 +0800

    feat: add menu btn click event

--- src/utils.ts
+++ src/utils.ts
@@ -11,3 +11,38 @@ export const sortedPost = async () => {
     }
   })
 }
+
+
+export const openNavBar = () => {
+  const navBar = document.getElementById("navBar");
+  const overlayer = document.getElementById("overlayer");
+  const menuBtns = document.querySelectorAll('.menu-btn')
+  menuBtns.forEach(menuBtn => {
+    const [menuIcon, closeIcon] = menuBtn.children;
+    menuIcon.classList.add("hidden")
+    closeIcon.classList.remove("hidden")
+  })
+
+  navBar?.classList.add("inset-y-0","left-0","shadow-lg")
+  navBar?.classList.replace("-translate-x-full","translate-x-0")
+
+  overlayer?.classList.replace("pointer-events-none", "pointer-events-auto");
+  overlayer?.classList.replace("opacity-0", "opacity-100");
+}
+
+export const closeNavBar = () => {
+  const navBar = document.getElementById("navBar");
+  const overlayer = document.getElementById("overlayer");
+  const menuBtns = document.querySelectorAll('.menu-btn')
+  menuBtns.forEach(menuBtn => {
+    const [menuIcon, closeIcon] = menuBtn.children;
+    menuIcon.classList.remove("hidden")
+    closeIcon.classList.add("hidden")
+  })
+
+  navBar?.classList.remove("inset-y-0","left-0","shadow-lg")
+  navBar?.classList.replace("translate-x-0","-translate-x-full")
+
+  overlayer?.classList.replace("pointer-events-auto", "pointer-events-none");
+  overlayer?.classList.replace("opacity-100", "opacity-0");
+}
