ef00f5873d9e4016ac3fc79b0046aa6cfea5e271
ReaJason <reajason1225@gmail.com>
2023-05-14 15:08:24 +0800

    fix: date format error

--- src/utils.ts
+++ src/utils.ts
@@ -1,49 +1,49 @@
 import { getCollection } from 'astro:content'
 
 export const sortedPost = async () => {
-  const blogs = await getCollection("blog");
-  return blogs.filter(blog => !blog.data.draft)
-  .filter(blog => !blog.data.hide)
-  .sort((a,b) => {
-    if(a.data?.sticky === b.data?.sticky){
+  const blogs = await getCollection('blog')
+  return blogs
+    .filter((blog) => !blog.data.draft)
+    .filter((blog) => !blog.data.hide)
+    .sort((a, b) => {
+      if (a.data?.sticky === b.data?.sticky) {
         return a.data?.date > b.data?.date ? -1 : 1
-    }else {
-      return (b.data?.sticky ?? 0) - (a.data?.sticky ?? 0)
-    }
-  })
+      } else {
+        return (b.data?.sticky ?? 0) - (a.data?.sticky ?? 0)
+      }
+    })
 }
 
-
 export const openNavBar = () => {
-  const navBar = document.getElementById("navBar");
-  const overlayer = document.getElementById("overlayer");
+  const navBar = document.getElementById('navBar')
+  const overlayer = document.getElementById('overlayer')
   const menuBtns = document.querySelectorAll('.menu-btn')
-  menuBtns.forEach(menuBtn => {
-    const [menuIcon, closeIcon] = menuBtn.children;
-    menuIcon.classList.add("hidden")
-    closeIcon.classList.remove("hidden")
+  menuBtns.forEach((menuBtn) => {
+    const [menuIcon, closeIcon] = menuBtn.children
+    menuIcon.classList.add('hidden')
+    closeIcon.classList.remove('hidden')
   })
 
-  navBar?.classList.add("inset-y-0","left-0","shadow-lg")
-  navBar?.classList.replace("-translate-x-full","translate-x-0")
+  navBar?.classList.add('inset-y-0', 'left-0', 'shadow-lg')
+  navBar?.classList.replace('-translate-x-full', 'translate-x-0')
 
-  overlayer?.classList.replace("pointer-events-none", "pointer-events-auto");
-  overlayer?.classList.replace("opacity-0", "opacity-100");
+  overlayer?.classList.replace('pointer-events-none', 'pointer-events-auto')
+  overlayer?.classList.replace('opacity-0', 'opacity-100')
 }
 
 export const closeNavBar = () => {
-  const navBar = document.getElementById("navBar");
-  const overlayer = document.getElementById("overlayer");
+  const navBar = document.getElementById('navBar')
+  const overlayer = document.getElementById('overlayer')
   const menuBtns = document.querySelectorAll('.menu-btn')
-  menuBtns.forEach(menuBtn => {
-    const [menuIcon, closeIcon] = menuBtn.children;
-    menuIcon.classList.remove("hidden")
-    closeIcon.classList.add("hidden")
+  menuBtns.forEach((menuBtn) => {
+    const [menuIcon, closeIcon] = menuBtn.children
+    menuIcon.classList.remove('hidden')
+    closeIcon.classList.add('hidden')
   })
 
-  navBar?.classList.remove("inset-y-0","left-0","shadow-lg")
-  navBar?.classList.replace("translate-x-0","-translate-x-full")
+  navBar?.classList.remove('inset-y-0', 'left-0', 'shadow-lg')
+  navBar?.classList.replace('translate-x-0', '-translate-x-full')
 
-  overlayer?.classList.replace("pointer-events-auto", "pointer-events-none");
-  overlayer?.classList.replace("opacity-100", "opacity-0");
+  overlayer?.classList.replace('pointer-events-auto', 'pointer-events-none')
+  overlayer?.classList.replace('opacity-100', 'opacity-0')
 }
