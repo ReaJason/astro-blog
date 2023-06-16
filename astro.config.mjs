f51203d6ac4bffa77fb65d195b5d52c5e517d5ae
ReaJason <reajason1225@gmail.com>
2023-06-17 01:08:58 +0800

    fix: site error

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.reajason.eu.org',
+  site: 'https://reajason.eu.org',
   integrations: [
     Unocss({
       injectReset: true,
