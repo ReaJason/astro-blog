9d5447472447df6216cdb353126bdc2108ba15da
ReaJason <reajason1225@gmail.com>
2023-07-15 23:19:38 +0800

    fix: date time white space

--- src/utils.ts
+++ src/utils.ts
@@ -49,7 +49,5 @@ export const closeNavBar = () => {
 }
 
 export const dateToString = (date: Date) => {
-  return `${date.getFullYear()}
-  -${(date.getMonth()+1).toString().padStart(2,'0')}
-  -${date.getDate().toString().padStart(2,'0')}`
+  return `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2,'0')}-${date.getDate().toString().padStart(2,'0')}`
 }
