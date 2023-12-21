001b34201bcb38df0556aad2d84bfc9ba8db4fa9
ReaJason <reajason1225@gmail.com>
2023-12-21 22:02:28 +0800

    feat: change avatar url

--- src/configs/site.ts
+++ src/configs/site.ts
@@ -1,8 +1,8 @@
 export const siteConfig = {
   title: "ReaJason's Space",
   author: "ReaJason",
-  avatar: "https://s.gravatar.com/avatar/5de6928d810a4b711580ff93fa1d409bfbc9cc4569fa6028b977f1bb6d39a850",
-  description: "keep moving",
+  avatar: "https://avatars.githubusercontent.com/u/48389642",
+  description: "Keep Moving✨",
   keywords: "ReaJason,BLog,Java,JavaScript,Python,RASP,JVM,后端",
   twitter: "@ReaJason_",
   github: "ReaJason",
