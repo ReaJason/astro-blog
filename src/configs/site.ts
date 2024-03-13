b2569f872cf154e978bb50f0c791fdaeeb8a4199
ReaJason <reajason1225@proton.me>
2024-03-13 23:24:17 +0800

    fix: type

--- src/configs/site.ts
+++ src/configs/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
   avatar: "https://s.gravatar.com/avatar/5de6928d810a4b711580ff93fa1d409bfbc9cc4569fa6028b977f1bb6d39a850",
   hompage: "https://cdn.jsdelivr.net/gh/ReaJason/blog_imgs/default/self_intro.png",
   description: "Keep Moving✨",
-  keywords: "ReaJason,BLog,Java,JavaScript,Python,RASP,JVM,后端",
+  keywords: "ReaJason,Blog,Java,JavaScript,Python,RASP,JVM,后端",
   twitter: "@ReaJason_",
   github: "ReaJason",
   repo: "ReaJason/astro-blog"
